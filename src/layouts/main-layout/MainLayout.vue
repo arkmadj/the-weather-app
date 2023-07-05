4f4a41f576cb668a4bda68ad87f142c5e72be4ae
arkmadj <ahmadjinadu@gmail.com>
2023-07-05 13:39:35 +0100

    added the main layout

---
+++ src/layouts/main-layout/MainLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="main-container">
+    <div class="side-bar"></div>
+    <section class="main-content">
+      <router-view/>
+    </section>
+  </div>
+</template>
+<script setup>
+</script>
+<style lang="scss" scoped>
+@import url("./MainLayout.scss");
+</style>
