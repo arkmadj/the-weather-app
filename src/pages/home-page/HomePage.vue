b073187844db16ea3d754a4a15a6d976b715f0ed
arkmadj <ahmadjinadu@gmail.com>
2023-07-05 14:36:40 +0100

    added weather card list

--- src/pages/home-page/HomePage.vue
+++ src/pages/home-page/HomePage.vue
@@ -12,8 +12,65 @@
       <div class="header__image"></div>
     </div>
   </nav>
+  <section class="weather-list">
+    <WeatherCard
+      v-for="(day, index) in days"
+      :key="index"
+      :day="day.day"
+      :type="day.type"
+      :high-temp="day.highTemp"
+      :low-temp="day.lowTemp"
+    />
+  </section>
 </template>
 <script setup>
+import { ref } from "vue";
+import WeatherCard from "../../components/weather-card/WeatherCard.vue";
+
+const days = ref([
+  {
+    day: "Sun",
+    type: "Rainy",
+    highTemp: 31,
+    lowTemp: 21,
+  },
+  {
+    day: "Mon",
+    type: "Sunny",
+    highTemp: 31,
+    lowTemp: 21,
+  },
+  {
+    day: "Tue",
+    type: "Rainy",
+    highTemp: 31,
+    lowTemp: 21,
+  },
+  {
+    day: "Wed",
+    type: "Rainy",
+    highTemp: 31,
+    lowTemp: 21,
+  },
+  {
+    day: "Thur",
+    type: "Rainy",
+    highTemp: 31,
+    lowTemp: 21,
+  },
+  {
+    day: "Fri",
+    type: "Rainy",
+    highTemp: 31,
+    lowTemp: 21,
+  },
+  {
+    day: "Sat",
+    type: "Rainy",
+    highTemp: 31,
+    lowTemp: 21,
+  },
+]);
 </script>
 <style lang="scss" scoped>
 @import "./HomePage.scss";
