80f6bdd9c967d03857914faae6fcf1fc5accae5a
arkmadj <ahmadjinadu@gmail.com>
2023-07-05 13:39:46 +0100

    added the home page header

---
+++ src/pages/home-page/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="header">
+    <div class="header__links">
+      <span class="header__link-item">Last 5 days</span>
+      <span class="header__link-item">Next 7 days</span>
+    </div>
+    <div class="header__right">
+      <div class="header__switch">
+        <div class="header__switch-item"></div>
+        <div class="header__switch-item"></div>
+      </div>
+      <div class="header__image"></div>
+    </div>
+  </nav>
+</template>
+<script setup>
+</script>
+<style lang="scss" scoped>
+@import "./HomePage.scss";
+</style>
