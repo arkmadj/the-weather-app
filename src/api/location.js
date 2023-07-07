68f5bda61421658febaf6cea66e3847d8f49e7f6
arkmadj <ahmadjinadu@gmail.com>
2023-07-08 00:54:45 +0100

    log env

--- src/api/location.js
+++ src/api/location.js
@@ -1,6 +1,7 @@
 import { axiosLocationInstance } from "../helpers/axiosConfig";
 
 const fetchCurrentLocation = async (lat, lon ) => {
+  console.log("Env", import.meta.env.VITE_GOOGLE_APP_KEY)
 	const response = await axiosLocationInstance.get(
 		`/maps/api/geocode/json?latlng=${lat},${lon}&key=AIzaSyAjwQBlPCN2pcy7dvH1MU5orEMx2s067N8`
 	);
