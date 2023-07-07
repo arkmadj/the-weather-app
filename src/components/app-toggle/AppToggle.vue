e47156952b8743c80833761e7a731576741a8787
arkmadj <ahmadjinadu@gmail.com>
2023-07-07 21:14:54 +0100

    added app toggle

---
+++ src/components/app-toggle/AppToggle.vue
@@ -0,0 +1,17 @@
+<template>
+  <input type="checkbox" v-model="checkbox" @change="handleCheck" />
+</template>
+<script setup>
+import { ref } from "vue";
+import useEventBus from "../../composables/eventBus";
+
+const { emit } = useEventBus();
+const checkbox = ref(false);
+
+const handleCheck = () => {
+  emit("showHighlights", checkbox.value);
+};
+</script>
+<style lang="scss" scoped>
+@import "./AppToggle.scss";
+</style>
