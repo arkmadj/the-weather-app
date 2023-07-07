da6bb4742b9afe6a4dbb551ac13253207d823f36
arkmadj <ahmadjinadu@gmail.com>
2023-07-07 23:46:19 +0100

    added get location from coords

--- src/layouts/main-layout/MainLayout.vue
+++ src/layouts/main-layout/MainLayout.vue
@@ -7,38 +7,42 @@
       <router-view />
     </section>
   </div>
-  <PageLoader v-else/>
+  <PageLoader v-else />
 </template>
 <script setup>
 import SideBar from "@/components/side-bar/SideBar.vue";
 import { onMounted, ref } from "vue";
 import useEventBus from "../../composables/eventBus";
 import PageLoader from "../../pages/page-loader/PageLoader.vue";
+import api from "@/api"
 
 const { emit: customEmit } = useEventBus();
 
-const loadingPage = ref(false)
+const loadingPage = ref(false);
 
 onMounted(() => {
-  loadingPage.value = true
   if (window.navigator.geolocation) {
-     window.navigator.geolocation.getCurrentPosition(fetchUserWeather, console.log);
+    window.navigator.geolocation.getCurrentPosition(fetchUserWeather, console.log);
   } else {
     fetchUserWeather({
       coords: { lat: 6.5243793, lon: 3.3792057 },
       city: "Lagos, Nigeria",
     });
   }
-  loadingPage.value = false
 });
 
-const fetchUserWeather = (position) => {
+const fetchUserWeather = async(position) => {
   customEmit("selectedLocation", {
     lat: position.coords.latitude,
     lon: position.coords.longitude,
-    city: "Lagos, Nigeria",
+    city: await getCoordinatesAddress(position.coords.latitude, position.coords.longitude),
   });
 };
+
+const getCoordinatesAddress = async (lat, lon) => {
+  const location = await api.location.fetchCurrentLocation(lat, lon)
+  return location.data.results[0].formatted_address || ""
+}
 </script>
 <style lang="scss" scoped>
 @import "./MainLayout.scss";
