f79582c3695a12d8cf5ee4c5b5ab3ce726d3c11d
arkmadj <ahmadjinadu@gmail.com>
2023-07-07 21:12:38 +0100

    added seaerch input with location autocomplete

--- src/components/search-input/SearchInput.vue
+++ src/components/search-input/SearchInput.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="select-container">
     <div class="input-container">
+      <component :is="SearchIcon" color="#929292" />
       <input type="text" placeholder="Search for places" class="main-input" ref="city" />
-      <img src="" alt="" class="input__icon" />
+      <component :is="TargetIcon" color="#929292" />
     </div>
     <div class="dropdown-container">
       <p></p>
@@ -10,22 +11,31 @@
   </div>
 </template>
 
-
-
 <script setup>
-import { onMounted, ref } from 'vue';
-const city = ref(null)
+import { onMounted, ref } from "vue";
+import SearchIcon from "../icons/SearchIcon.vue";
+import TargetIcon from "../icons/TargetIcon.vue";
+import useEventBus from "../../composables/eventBus";
 
+const city = ref(null);
+const { emit: customEmit } = useEventBus();
 
 onMounted(() => {
-  const cityAutocomplete = new google.maps.places.Autocomplete(
-    city.value
-  );
+  const cityAutocomplete = new google.maps.places.Autocomplete(city.value);
 
-  cityAutocomplete.setTypes(["(cities)"])
-})
+  cityAutocomplete.setTypes(["(cities)"]);
 
+  cityAutocomplete.addListener("place_changed", () => {
+    const place = cityAutocomplete.getPlace();
+
+    customEmit("selectedLocation", {
+      lat: place.geometry.location.lat(),
+      lon: place.geometry.location.lng(),
+      city: place.formatted_address,
+    });
+  });
+});
 </script>
 <style lang="scss" scoped>
-@import url("./SearchInput.scss");
+@import "./SearchInput.scss";
 </style>
