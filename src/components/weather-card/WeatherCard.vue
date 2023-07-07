7f6f6c7312f4b2d22f737253cadff1c9dc625e67
arkmadj <ahmadjinadu@gmail.com>
2023-07-07 21:20:23 +0100

    added weather card

--- src/components/weather-card/WeatherCard.vue
+++ src/components/weather-card/WeatherCard.vue
@@ -1,23 +1,31 @@
 <template>
   <div class="card">
-    <p class="card__day">{{day}}</p>
+    <p class="card__day">{{ day }}</p>
     <div class="card__image-container">
-      <div class="card__image">{{ type }}</div>
+      <img
+        :src="`https://openweathermap.org/img/wn/${type}@4x.png`"
+        alt="Weather Image"
+        class="card__image"
+      />
     </div>
     <div class="card__footer">
-      <span class="card__high-temp">{{ highTemp }}ºC</span>
-      <span class="card__low-temp">{{ lowTemp }}ºF</span>
+      <span v-if="isHistory">{{ temp }}ºC</span>
+      <span class="card__high-temp" v-if="!isHistory">{{ highTemp }}ºC</span>
+      <span class="card__low-temp" v-if="!isHistory">{{ lowTemp }}ºC</span>
     </div>
   </div>
 </template>
 <script setup>
 
 const props = defineProps({
-  day: String,
-  type: String,
-  highTemp: String,
-  lowTemp: String
-})
+  day: { type: String, default: "Sun" },
+  type: { type: String, default: "10d" },
+  highTemp: { type: Number, default: 36 },
+  lowTemp: { type: Number, default: 25 },
+  temp: { type: Number, default: 25},
+  isHistory: {type: Boolean, default: false}
+});
+
 </script>
 <style lang="scss" scoped>
 @import "./WeatherCard.scss";
