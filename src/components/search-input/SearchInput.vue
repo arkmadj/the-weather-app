2ef8f20764dbc4d4d05af600b90fbebd1394cd2a
arkmadj <ahmadjinadu@gmail.com>
2023-07-07 23:45:04 +0100

    added hover  on target

--- src/components/search-input/SearchInput.vue
+++ src/components/search-input/SearchInput.vue
@@ -3,10 +3,7 @@
     <div class="input-container">
       <component :is="SearchIcon" color="#929292" />
       <input type="text" placeholder="Search for places" class="main-input" ref="city" />
-      <component :is="TargetIcon" color="#929292" />
-    </div>
-    <div class="dropdown-container">
-      <p></p>
+      <component :is="TargetIcon" color="#929292" @click="getLocation" class="input-target" />
     </div>
   </div>
 </template>
@@ -16,6 +13,7 @@ import { onMounted, ref } from "vue";
 import SearchIcon from "../icons/SearchIcon.vue";
 import TargetIcon from "../icons/TargetIcon.vue";
 import useEventBus from "../../composables/eventBus";
+import api from "@/api"
 
 const city = ref(null);
 const { emit: customEmit } = useEventBus();
@@ -35,6 +33,31 @@ onMounted(() => {
     });
   });
 });
+
+const getLocation = () => {
+  if (window.navigator.geolocation) {
+    window.navigator.geolocation.getCurrentPosition(fetchUserWeather, console.log);
+  } else {
+    fetchUserWeather({
+      coords: { lat: 6.5243793, lon: 3.3792057 },
+      city: "Lagos, Nigeria",
+    });
+  }
+}
+
+const fetchUserWeather = async(position) => {
+  customEmit("selectedLocation", {
+    lat: position.coords.latitude,
+    lon: position.coords.longitude,
+    city: await getCoordinatesAddress(position.coords.latitude, position.coords.longitude),
+  });
+};
+
+const getCoordinatesAddress = async (lat, lon) => {
+  const location = await api.location.fetchCurrentLocation(lat, lon)
+  return location.data.results[0].formatted_address || ""
+}
+
 </script>
 <style lang="scss" scoped>
 @import "./SearchInput.scss";
