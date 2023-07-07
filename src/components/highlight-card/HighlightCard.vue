f6d95aecd27e32a128832bf847653e52b58a6530
arkmadj <ahmadjinadu@gmail.com>
2023-07-07 21:18:25 +0100

    added highlight card

---
+++ src/components/highlight-card/HighlightCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="highlight-card">
+    <span class="highlight-card__title">{{ title }}</span>
+    
+    <div class="value-container">
+      <slot name="value-body"></slot>
+    </div>
+    <p>{{ description }}</p>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  title: String,
+  value: String,
+  description: String,
+});
+</script>
+<style lang="scss" scoped>
+@import "./HighlightCard.scss";
+</style>
