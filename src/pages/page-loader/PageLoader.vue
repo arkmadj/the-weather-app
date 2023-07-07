1cb31995857e891485061fb4cfc5e4d9a0030420
arkmadj <ahmadjinadu@gmail.com>
2023-07-07 21:21:30 +0100

    added page loader

---
+++ src/pages/page-loader/PageLoader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="loader-container">
+    <AppLoader/>
+  </div>
+</template>
+<script setup>
+import AppLoader from "../../components/app-loader/AppLoader.vue";
+</script>
+<style lang="scss" scoped>
+@import "./PageLoader.scss";
+</style>
