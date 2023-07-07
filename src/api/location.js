cff93f387b8281c70978ef10565e253df479494d
arkmadj <ahmadjinadu@gmail.com>
2023-07-08 00:45:55 +0100

    removed env

--- src/api/location.js
+++ src/api/location.js
@@ -2,7 +2,7 @@ import { axiosLocationInstance } from "../helpers/axiosConfig";
 
 const fetchCurrentLocation = async (lat, lon ) => {
 	const response = await axiosLocationInstance.get(
-		`/maps/api/geocode/json?latlng=${lat},${lon}&key=${import.meta.env.VITE_GOOGLE_APP_KEY}`
+		`/maps/api/geocode/json?latlng=${lat},${lon}&key=AIzaSyAjwQBlPCN2pcy7dvH1MU5orEMx2s067N8`
 	);
 	return response;
 };
--- src/api/weather.js
+++ src/api/weather.js
@@ -2,21 +2,21 @@ import { axiosInstance } from "../helpers/axiosConfig";
 
 const fetchCurrentWeather = async ({ lat, lon }) => {
 	const response = await axiosInstance.get(
-		`/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=metric&appid=${import.meta.env.VITE_OPEN_WEATHER_API_KEY}`
+		`/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=metric&appid=bd5e378503939ddaee76f12ad7a97608`
 	);
 	return response;
 };
 
 const fetchWeatherForecast = async ({ lat, lon }) => {
 	const response = await axiosInstance.get(
-		`/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts,current&units=metric&appid=${import.meta.env.VITE_OPEN_WEATHER_API_KEY}`
+		`/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts,current&units=metric&appid=bd5e378503939ddaee76f12ad7a97608`
 	);
 	return response;
 };
 
 const fetchWeatherHistory = async ({ lat, lon, date }) => {
 	const response = await axiosInstance.get(
-		`/data/2.5/onecall/timemachine?lat=${lat}&lon=${lon}&dt=${date}&appid=${import.meta.env.VITE_OPEN_WEATHER_API_KEY}&only_current={true}`
+		`/data/2.5/onecall/timemachine?lat=${lat}&lon=${lon}&dt=${date}&appid=bd5e378503939ddaee76f12ad7a97608&only_current={true}`
 	);
 	return response;
 };
@@ -25,4 +25,5 @@ export default {
 	fetchCurrentWeather,
 	fetchWeatherForecast,
 	fetchWeatherHistory,
+	fetchCurrentWeatherByCoords,
 };
