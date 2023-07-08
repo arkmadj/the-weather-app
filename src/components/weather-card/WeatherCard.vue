62420dde9e0852508f6a06340d9c532a2d7858c1
arkmadj <ahmadjinadu@gmail.com>
2023-07-08 13:16:41 +0100

    change temp props type to string

--- src/components/weather-card/WeatherCard.vue
+++ src/components/weather-card/WeatherCard.vue
@@ -9,9 +9,9 @@
       />
     </div>
     <div class="card__footer">
-      <span v-if="isHistory">{{ temp }}ºC</span>
-      <span class="card__high-temp" v-if="!isHistory">{{ highTemp }}ºC</span>
-      <span class="card__low-temp" v-if="!isHistory">{{ lowTemp }}ºC</span>
+      <span v-if="isHistory">{{ temp }}</span>
+      <span class="card__high-temp" v-if="!isHistory">{{ highTemp }}</span>
+      <span class="card__low-temp" v-if="!isHistory">{{ lowTemp }}</span>
     </div>
   </div>
 </template>
@@ -20,9 +20,9 @@
 const props = defineProps({
   day: { type: String, default: "Sun" },
   type: { type: String, default: "10d" },
-  highTemp: { type: Number, default: 36 },
-  lowTemp: { type: Number, default: 25 },
-  temp: { type: Number, default: 25},
+  highTemp: { type: String, default: "36" },
+  lowTemp: { type: String, default: "25" },
+  temp: { type: String, default: "25"},
   isHistory: {type: Boolean, default: false}
 });
 
