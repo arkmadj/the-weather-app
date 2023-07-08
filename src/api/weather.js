8922058ec780ae4038dcb388ed8d6902e704c1ac
arkmadj <ahmadjinadu@gmail.com>
2023-07-08 01:00:58 +0100

    added env

--- src/api/weather.js
+++ src/api/weather.js
@@ -2,21 +2,21 @@ import { axiosInstance } from "../helpers/axiosConfig";
 
 const fetchCurrentWeather = async ({ lat, lon }) => {
 	const response = await axiosInstance.get(
-		`/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=metric&appid=bd5e378503939ddaee76f12ad7a97608`
+		`/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=metric&appid=7b26c92417fd3678d52eac12dc870222`
 	);
 	return response;
 };
 
 const fetchWeatherForecast = async ({ lat, lon }) => {
 	const response = await axiosInstance.get(
-		`/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts,current&units=metric&appid=bd5e378503939ddaee76f12ad7a97608`
+		`/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts,current&units=metric&appid=7b26c92417fd3678d52eac12dc870222`
 	);
 	return response;
 };
 
 const fetchWeatherHistory = async ({ lat, lon, date }) => {
 	const response = await axiosInstance.get(
-		`/data/2.5/onecall/timemachine?lat=${lat}&lon=${lon}&dt=${date}&appid=bd5e378503939ddaee76f12ad7a97608&only_current={true}`
+		`/data/2.5/onecall/timemachine?lat=${lat}&lon=${lon}&dt=${date}&appid=7b26c92417fd3678d52eac12dc870222&only_current={true}`
 	);
 	return response;
 };
