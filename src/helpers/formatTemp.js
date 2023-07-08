cafcfb7d7c338b7247697a84364bbb5bc4f1169c
arkmadj <ahmadjinadu@gmail.com>
2023-07-08 13:18:07 +0100

    added format temperature

---
+++ src/helpers/formatTemp.js
@@ -0,0 +1,8 @@
+export const formatTemp = (val, unit, showUnit = false) => {
+	const suffixUnit = showUnit ? (val === "celsius" ? "ºC" : "ºF") : "";
+	if (unit === "celsius") {
+		return parseInt(val) + suffixUnit;
+	}
+
+	return parseInt(val * (9 / 5) + 32) + suffixUnit;
+};
