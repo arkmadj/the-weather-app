410ef0c4c85b1f9d6cff9a4eb9da53cab8f0bfa6
arkmadj <ahmadjinadu@gmail.com>
2023-07-11 14:11:26 +0100

    added store and remove event bus emits

--- src/components/search-input/SearchInput.vue
+++ src/components/search-input/SearchInput.vue
@@ -2,8 +2,18 @@
   <div class="select-container">
     <div class="input-container">
       <component :is="SearchIcon" color="#929292" />
-      <input type="text" placeholder="Search for places" class="main-input" ref="city" />
-      <component :is="TargetIcon" color="#929292" @click="getLocation" class="input-target" />
+      <input
+        type="text"
+        placeholder="Search for places"
+        class="main-input"
+        ref="city"
+      />
+      <component
+        :is="TargetIcon"
+        color="#929292"
+        @click="getLocation"
+        class="input-target"
+      />
     </div>
   </div>
 </template>
@@ -12,11 +22,18 @@
 import { onMounted, ref } from "vue";
 import SearchIcon from "../icons/SearchIcon.vue";
 import TargetIcon from "../icons/TargetIcon.vue";
-import useEventBus from "../../composables/eventBus";
-import api from "@/api"
+import { useStore } from "vuex";
+
+const emits = defineEmits({
+  selectedLocation(payload) {
+    if (Object.keys(payload).length) return true;
+    return false;
+  },
+});
 
 const city = ref(null);
-const { emit: customEmit } = useEventBus();
+
+const store = useStore();
 
 onMounted(() => {
   const cityAutocomplete = new google.maps.places.Autocomplete(city.value);
@@ -26,38 +43,34 @@ onMounted(() => {
   cityAutocomplete.addListener("place_changed", () => {
     const place = cityAutocomplete.getPlace();
 
-    customEmit("selectedLocation", {
-      lat: place.geometry.location.lat(),
-      lon: place.geometry.location.lng(),
-      city: place.formatted_address,
+    fetchUserWeather({
+      coords: {
+        latitude: place.geometry.location.lat(),
+        longitude: place.geometry.location.lng(),
+      },
     });
   });
 });
 
-const getLocation = () => {
+const getLocation = async () => {
   if (window.navigator.geolocation) {
     window.navigator.geolocation.getCurrentPosition(fetchUserWeather, console.log);
   } else {
     fetchUserWeather({
-      coords: { lat: 6.5243793, lon: 3.3792057 },
-      city: "Lagos, Nigeria",
+      coords: { latitude: 6.5243793, longitude: 3.3792057 },
     });
   }
-}
-
-const fetchUserWeather = async(position) => {
-  customEmit("selectedLocation", {
-    lat: position.coords.latitude,
-    lon: position.coords.longitude,
-    city: await getCoordinatesAddress(position.coords.latitude, position.coords.longitude),
-  });
 };
 
-const getCoordinatesAddress = async (lat, lon) => {
-  const location = await api.location.fetchCurrentLocation(lat, lon)
-  return location.data.results[0].formatted_address || ""
-}
+const fetchUserWeather = async (position) => {
+  store.commit("app/setLoading", true);
 
+  emits("selectedLocation", {
+    lat: position.coords.latitude,
+    lon: position.coords.longitude,
+  });
+
+};
 </script>
 <style lang="scss" scoped>
 @import "./SearchInput.scss";
